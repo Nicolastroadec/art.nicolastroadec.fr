7763fc2d1ff3c5bea91825b91471c271c9cdf530
Nicolastroadec <troadecnicolas@hotmail.fr>
2024-08-28 15:03:29 +0200

    type cart

--- app/(ui)/components/CartIcon.tsx
+++ app/(ui)/components/CartIcon.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 
-export default function CartIcon({ cart }) {
+export default function CartIcon({ cart }: { cart: any }) {
     return (
         <>
             {cart.cart.length}<Image width="20" height="20" className="w-[20px]" src='/img/cart-icon.png' alt="Panier" />
