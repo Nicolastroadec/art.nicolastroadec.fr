2aa453fbb5acc817b4e093dec1161422edfef1d8
Nicolastroadec <troadecnicolas@hotmail.fr>
2024-09-05 09:30:31 +0200

    path to @components/backoffice/TableProducts

--- app/backoffice/page.tsx
+++ app/backoffice/page.tsx
@@ -1,7 +1,7 @@
 import AddAProduct from '@components/backoffice/AddAProduct';
 import Product from '@models/Product';
 import getProducts from '@lib/get-data';
-import TableProducts from '@app/(ui)/components/backoffice/TableProducts';
+import TableProducts from '@components/backoffice/TableProducts';
 
 export default async function page() {
     let artItems: Product[] = [];
