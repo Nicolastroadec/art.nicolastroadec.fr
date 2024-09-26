94235b8f4edbf81855b8e5527834c26baed1d190
Nicolastroadec <troadecnicolas@hotmail.fr>
2024-09-26 17:43:32 +0200

    update

--- app/produit/[slug]/page.tsx
+++ app/produit/[slug]/page.tsx
@@ -2,7 +2,6 @@ import { getProduct } from '@lib/get-data';
 import Image from 'next/image';
 import Product from '@models/Product';
 import ProductUI from '@components/productUI';
-import { CartProvider } from '@context/CartContext'
 
 export default async function page({ params }: { params: { slug: string } }) {
     const slug = params.slug;
